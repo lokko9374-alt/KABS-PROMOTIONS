ca44844403f1763d3571b66dca6b3a3c5aa0fd81
lokko9374-alt <lokko9374@gmail.com>
2025-12-16 15:55:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +10,3 @@ export default defineConfig({
     },
   },
 });
-
